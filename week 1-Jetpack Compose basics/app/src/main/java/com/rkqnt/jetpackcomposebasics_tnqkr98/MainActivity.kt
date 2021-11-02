0a57e62e864ea180136590f1dbe1afee92523856
DongHan <tnqkr98@naver.com>
2021-11-02 23:16:35 +0900

    [tnqkr98]Update MainActivity.kt

--- week 1-Jetpack Compose basics/app/src/main/java/com/rkqnt/jetpackcomposebasics_tnqkr98/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/rkqnt/jetpackcomposebasics_tnqkr98/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.rkqnt.jetpackcomposebasics_tnqkr98
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rkqnt.jetpackcomposebasics_tnqkr98.ui.theme.JetpackComposeBasics_tnqkr98Theme
 
 class MainActivity : ComponentActivity() {
@@ -16,11 +32,92 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeBasics_tnqkr98Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            //    Surface(color = MaterialTheme.colors.background) {
+                Conversation(messages = SampleData.conversationSample)
+                    //Greeting("Android")
+           //     }
             }
         }
+    }
+}
+
+data class Message(val author : String, var body : String)
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard(){
+    JetpackComposeBasics_tnqkr98Theme {
+        MessageCard(Message("Android", "JetPack Compose"))
+    }
+}
+
+@Composable
+fun MessageCard(msg : Message){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription ="Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // isExpanded 는 boolean 변수로 remember를 통해 상태를 메모리에 저장. 최초 false 저장
+        // 여기서 mutableStateOf는 상태가 변화하는지 추적
+        var isExpanded by remember { mutableStateOf(false)}
+
+        // (애니메이션)변화하는 색상 선언, (false) primary -> surface, (true) 반대
+        val surfaceColor: Color by animateColorAsState(
+            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded}) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,   // 색상 애니메이션
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,   // isExpanded 가 false이면 한줄만 보여줌
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages){ message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    JetpackComposeBasics_tnqkr98Theme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
 
@@ -33,6 +130,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasics_tnqkr98Theme {
-        Greeting("Android")
+        //Greeting("Android")
     }
 }
