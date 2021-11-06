1de8efdc075fba03adc8101f86935244df80a767
DongHan <tnqkr98@naver.com>
2021-11-06 21:57:36 +0900

    Add JetpackComposeBasics at CodeLab

---
+++ week 1-Jetpack Compose basics/JetPackComposeBasics_CodeLab/app/src/main/java/com/rkqnt/jetpackcomposebasics/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rkqnt.jetpackcomposebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rkqnt.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBasicsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBasicsTheme {
+        Greeting("Android")
+    }
+}
---
+++ week 1-Jetpack Compose basics/app/src/main/java/com/rkqnt/jetpackcomposebasics_tutorial/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.rkqnt.jetpackcomposebasics_tutorial
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rkqnt.jetpackcomposebasics_tutorial.ui.theme.JetpackComposeBasics_tnqkr98Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeBasics_tnqkr98Theme {
+                // A surface container using the 'background' color from the theme
+            //    Surface(color = MaterialTheme.colors.background) {
+                Conversation(messages = SampleData.conversationSample)
+                    //Greeting("Android")
+           //     }
+            }
+        }
+    }
+}
+
+data class Message(val author : String, var body : String)
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable                 // Composable Function
+fun PreviewMessageCard(){
+    JetpackComposeBasics_tnqkr98Theme {
+        MessageCard(Message("Android", "JetPack Compose"))
+    }
+}
+
+// Modifier 가 기존 XML 에서 속성
+
+@Composable
+fun MessageCard(msg : Message){
+    // Layout Composable : Row(Linear-Hori), Column(Linear-Verti), Box (FrameLayout)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription ="Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // isExpanded 는 boolean 변수로 remember를 통해 상태를 메모리에 저장. 최초 false 저장
+        // 여기서 mutableStateOf는 상태가 변화하는지 추적
+        var isExpanded by remember { mutableStateOf(false)}
+
+        // (애니메이션)변화하는 색상 선언, (false) primary -> surface, (true) 반대
+        val surfaceColor: Color by animateColorAsState(
+            if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded}) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,   // 색상 애니메이션
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if(isExpanded) Int.MAX_VALUE else 1,   // isExpanded 가 false이면 한줄만 보여줌
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    // List 형태를 구현해주는 Composable
+    LazyColumn{
+        items(messages){ message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    JetpackComposeBasics_tnqkr98Theme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeBasics_tnqkr98Theme {
+        //Greeting("Android")
+    }
+}
