0539903d675ae03140130c4f4153410d69ccf7e2
DongHan <tnqkr98@naver.com>
2021-11-19 19:39:03 +0900

    week1 complete

--- week 1-Jetpack Compose basics/JetPackComposeBasics_CodeLab/app/src/main/java/com/rkqnt/jetpackcomposebasics/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/JetPackComposeBasics_CodeLab/app/src/main/java/com/rkqnt/jetpackcomposebasics/ui/theme/Theme.kt
@@ -2,21 +2,25 @@ package com.rkqnt.jetpackcomposebasics.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
@@ -42,6 +46,25 @@ fun JetpackComposeBasicsTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
         content = content
     )
 }
